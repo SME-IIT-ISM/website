381b7667404ef96ed4b8d64a4fa7f12afc815401
Knight <geetaabhinav2001@gmail.com>
2021-06-04 17:32:41 +0530

    added counter

--- client/src/components/OverlayText.js
+++ client/src/components/OverlayText.js
@@ -1,13 +1,15 @@
 import React from "react";
 import khananImage from "../image/khanan.jpg";
+import Countdown from "./Countdown";
 
 const OverlayText = (props) => {
   return (
-      <div class="img-container">
-        <img src={khananImage} alt="" />
-        <div class="overlay">
-          <span>overlay content</span>
-        </div>
+    <div class="img-container">
+      <img src={khananImage} alt="" />
+      <div class="overlay">
+        <span>overlay content</span>
+        <Countdown />
+      </div>
     </div>
   );
 };
