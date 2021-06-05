9cf52825d29132c5dbacf47ccf73c23472270fc2
Knight <geetaabhinav2001@gmail.com>
2021-06-05 21:42:58 +0530

    updated overlaytext

--- client/src/components/Home/OverlayText.js
+++ client/src/components/Home/OverlayText.js
@@ -5,32 +5,35 @@ import { Container, Row, Col } from "reactstrap";
 
 const OverlayText = (props) => {
   return (
-    <Container>
-      <Row>
-        <Col className="img-container mb-2">
-          <img src={khananImage} alt="" />
-        </Col>
-        <Col>
-          <div classname="mt-3">
-            <p className="h2 pt-3">
-              Participate in the Biggest Mining fest in India and fell into the
-              invigorating world of Mining.
-            </p>
-            <p className="h2">
-              You will be provided with a platform that presents your talent to
-              the world and help you enhance and learn skills. Many esteemed
-              guests are willing to share their knowledge are you willing to
-              take it?
-            </p>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="text-center h0">
-          <Countdown />
-        </Col>
-      </Row>
-    </Container>
+    <div className="khanan-overlay py-5">
+      <h1 className="text-white text-center text-uppercase">khanan</h1>
+      <Container className="p-4 rounded ">
+        <Row>
+          <Col className="img-container mb-1">
+            <img src={khananImage} alt="" />
+          </Col>
+          <Col className="text-white">
+            <div classname="mt-3">
+              <p className="h2 pt-3">
+                Participate in the Biggest Mining fest in India and fell into
+                the invigorating world of Mining.
+              </p>
+              <p className="h2">
+                You will be provided with a platform that presents your talent
+                to the world and help you enhance and learn skills. Many
+                esteemed guests are willing to share their knowledge are you
+                willing to take it?
+              </p>
+            </div>
+          </Col>
+        </Row>
+        {/* <Row>
+          <Col className="text-center h1">
+            <Countdown />
+          </Col>
+        </Row> */}
+      </Container>
+    </div>
   );
 };
 
