0d9d45798192bc680adb907dbf0539c386f8b848
Knight <geetaabhinav2001@gmail.com>
2021-06-06 16:53:14 +0530

    added Youtube.jsx

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -23,7 +23,7 @@ const Header = () => {
 
   return (
     <div>
-      <Navbar expand="md" color="dark" dark fixed="top">
+      <Navbar expand="md" color="light" light fixed="top">
         <NavbarBrand className="mx-4 my-2" href="/">
           <img src={Logo} className="d-inline-block align-top" alt="Logo" />
         </NavbarBrand>
--- client/src/components/Home/OverlayText.js
+++ client/src/components/Home/OverlayText.js
@@ -6,13 +6,15 @@ import { Container, Row, Col } from "reactstrap";
 const OverlayText = (props) => {
   return (
     <div className="khanan-overlay py-5">
-      <h1 className="text-white text-center text-uppercase">khanan</h1>
-      <Container className="p-4 rounded ">
+      <h1 className="text-white text-center text-uppercase khanan-content">
+        khanan
+      </h1>
+      <Container className="p-4 rounded khanan-content">
         <Row>
           <Col className="img-container mb-1">
             <img src={khananImage} alt="" />
           </Col>
-          <Col className="text-white">
+          <Col className="text-white ">
             <div classname="mt-3">
               <p className="h2 pt-3">
                 Participate in the Biggest Mining fest in India and fell into
