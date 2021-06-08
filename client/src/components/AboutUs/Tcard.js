c36b15013809c53b8159bac40793d7808fb6c5f6
Knight <geetaabhinav2001@gmail.com>
2021-06-08 12:41:33 +0530

    update timeline

--- client/src/components/AboutUs/Tcard.js
+++ client/src/components/AboutUs/Tcard.js
@@ -13,15 +13,15 @@ const Tcard = (props) => {
         <span class="timeline__year time" aria-hidden="true">
           {year}
         </span>
-        <div class="timeline__card card">
-          <header class="card__header">
+        <div class="timeline__card tcard">
+          <header class="tcard__header">
             <time class="time">
               <span class="time__day">{day}</span>
               <span class="time__month">{month}</span>
             </time>
-            <h3 class="card__title r-title">{title}</h3>
+            <h3 class="tcard__title r-title">{title}</h3>
           </header>
-          <div class="card__content">
+          <div class="tcard__content">
             <p>{achievement}</p>
           </div>
         </div>
