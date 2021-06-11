8d082c45187de4fbb7e9ae5058092194f7984f28
Abhijeet Haldar <abhijeethaldar2001@gmail.com>
2021-06-11 16:23:47 +0530

    Gallery.jsIntroCompleted

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -3,7 +3,7 @@ import "./gallery.css";
 import GHImage1 from "./BackgroundGalleryA.jpg";
 import GHImage2 from "./BackgroundGalleryB.jpg";
 import GHImage3 from "./BackgroundGalleryC.jpg";
-import GHImage4 from "./BackgroundGalleryD.jpg";
+
 import {
   Card,
   CardImg,
@@ -20,42 +20,65 @@ const Gallery = () => {
       <div className="heading">
         <h1 className="text-uppercase text-center">Gallery</h1>
       </div>
-      <div className="GH-image">
-     
-        <Card className="GH-card">
-          <CardImg top width="100%" src={GHImage1} alt="Card image cap" />
-          <CardBody>
-            <CardTitle tag="h5">Indian Mining Day Celebration</CardTitle>
-          </CardBody>
-        </Card>
-<div className="row GH-card-All">
-<div className="col-sm">
-        <Card className="GH-card-compo1">
-          <CardImg top width="100%" src={GHImage2} alt="Card image cap" />
-          <CardBody>
-            <CardTitle tag="h5">Indian Mining Day Celebration</CardTitle>
-          </CardBody>
-        </Card>
+      <div className=" container GH-intro">
+        <div className="row">
+          <div className="col-sm">
+            <Card className="GHImage1">
+              <CardImg
+                className="GH"
+                top
+                width="100%"
+                src={GHImage1}
+                alt="Card image cap"
+              />
+              <CardBody className=" Imagetext">
+                <CardTitle tag="h5">National Mining Day</CardTitle>
+              </CardBody>
+            </Card>
+          </div>
+          <div className="col-sm">
+            <div className="row GHText">
+              <p className="h1 text-white  GHText1">Welcome to our Memories</p>
+              <p className="h4 text-white display-6 GHText2">
+                Take a trip down our memory lane
+              </p>
+            </div>
+            <div className="row GHSmall">
+              <div className="col-sm">
+                <Card className="GHImage2">
+                  <CardImg
+                    className="GH"
+                    top
+                    width="100%"
+                    src={GHImage2}
+                    alt="Card image cap"
+                  />
+                  <CardBody className=" Imagetext">
+                    <CardTitle tag="h5">SME</CardTitle>
+                  </CardBody>
+                </Card>
+              </div>
+              <div className="col-sm">
+                <Card className="GHImage3">
+                  <CardImg
+                    className="GH"
+                    top
+                    width="100%"
+                    src={GHImage3}
+                    alt="Card image cap"
+                  />
+                  <CardBody className="Imagetext">
+                    <CardTitle tag="h5">Diwali</CardTitle>
+                  </CardBody>
+                </Card>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="col-sm">
-        <Card className="GH-card-compo2">
-          <CardImg top width="100%" src={GHImage3} alt="Card image cap" />
-          <CardBody>
-            <CardTitle tag="h5">Diwali</CardTitle>
-          </CardBody>
-        </Card>
-        </div>
-        <div className="col-sm">
-        <Card className="GH-card-compo3">
-          <CardImg top width="100%" src={GHImage4} alt="Card image cap" />
-          <CardBody>
-            <CardTitle tag="h5">Alumni Meet</CardTitle>
-          </CardBody>
-        </Card>
-        </div>
+        <div className="row my-2 ">
+
         </div>
       </div>
-      
     </div>
   );
 };
