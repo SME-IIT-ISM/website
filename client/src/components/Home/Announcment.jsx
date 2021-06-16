9ee1b21109231478c7f964d4f6de59af43c9a5fb
Kartik Saini <kartik100saini@gmail.com>
2021-06-16 16:11:10 +0530

    Update announcement section

--- client/src/components/Home/Announcment.jsx
+++ client/src/components/Home/Announcment.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import Marquee from "react-easy-marquee";
 
 const Announcement = () => {
+  const data = [
+    "Announcement 8",
+    "Announcement 7",
+    "Announcement 6",
+    "Announcement 5",
+    "Announcement 4",
+    "Announcement 3",
+    "Announcement 2",
+    "Announcement 1",
+  ];
+
   return (
-    <div className="marquee-container">
-      <div>
-        <h1 className="heading text-uppercase text-center">Announcements</h1>
-      </div>
-      <div className="marquee-container">
-        <div className="own-marquee">
-          <b>
-            <p>first</p>
-            <br />
-            <p>Second</p>
-          </b>
-        </div>
+    <div>
+      <h1 className="heading text-uppercase text-center">Announcements</h1>
+      <div className="py-3">
+        <Marquee
+          duration={10000}
+          background="#fff"
+          height="350px"
+          width="100%"
+          axis="Y"
+          align="center"
+          pauseOnHover={true}
+          reverse={true}
+        >
+          {data.map((item, id) => (
+            <p key={id}>
+              <b>{item}</b>
+            </p>
+          ))}
+        </Marquee>
       </div>
     </div>
   );
