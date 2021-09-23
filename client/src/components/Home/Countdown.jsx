c95f8469da44415c4053dfcc4aeb4898c0f48a5b
Knight <geetaabhinav2001@gmail.com>
2021-09-23 23:18:52 +0530

    updated countdown

--- client/src/components/Home/Countdown.jsx
+++ client/src/components/Home/Countdown.jsx
@@ -10,7 +10,8 @@ function Countdown() {
       timeLeft = {
         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
         hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60)
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
       };
     }
 
